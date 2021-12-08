89c6f75f9b51dca0ca06247d6d4368c2a06f58b5
Herlana <hrlana1312@gmail.com>
2021-12-08 20:44:17 +0700

    chore: delete routes Mata Kuliah

--- service-penilaian/src/routes/Mata Kuliah.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import * as MataKuliahController from '../controller/Mata Kuliah'
-import * as ValidatorSanitizer from '../middleware/InputValidatorSanitizer'
-
-const router = express.Router()
-
-export default router
