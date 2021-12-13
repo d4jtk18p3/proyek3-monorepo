b1837484cef236c7b504a447c61df2aadb15eaeb
fachdf <fachridhia.f@gmail.com>
2021-12-13 07:51:58 +0700

    Revert "DevOps - Benerin Route utk Deployment"
    This reverts commit 5bcc5f9830b397410b88fb7c8b07990cb14a4dc3.

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   devServer: {
     port: 14415,
-    proxy:{
-      '/api/penilaian':{
-          target: 'http://localhost:5002'
-      },
-      '/api/absensi' :{
-          target: 'http://localhost:5001'
-      }
-    },
     allowedHosts: [
       "akun.localhost",
       "frontend.localhost"
